b003f361a8fefa56afa20eff656deaceb4375094
asnadya <sharmathecomp@gmail.com>
2020-01-27 22:04:15 +0530

    mailer

--- assets/contact.php
+++ assets/contact.php
@@ -1,10 +1,13 @@
 <?php
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://sochme.com/mailer.php?name=".$_GET["name"]."&email=".$_GET["email"]."&mobile=".$_GET["mobile"]."&message=".$_GET[message]);
-curl_setopt($ch, CURLOPT_HEADER, 0);
-curl_exec($ch);
-curl_close($ch);
-header("Location: https://www.dmsol.in?mail=submitted");
-exit();
+$url = "https://sochme.com/mailer.php?name=".$_GET["name"]."&email=".$_GET["email"]."&mobile=".$_GET["mobile"]."&message=".$_GET[message];
+echo $url;
+//curl_setopt($ch, CURLOPT_URL, $url);
+// 
+//curl_setopt($ch, CURLOPT_HEADER, 0);
+//curl_exec($ch);
+//curl_close($ch);
+//header("Location: https://www.dmsol.in?mail=submitted");
+//exit();
 ?>
 	
